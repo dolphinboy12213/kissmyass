bb2b55c21c8e6ea42c2726ac86c1f57f64cccc24
TheDolphin <dolphinboy12213@gmail.com>
2024-01-04 14:51:49 -0600

    Help Aahan’s privacy?
    Took Aahans name out

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import TypingIndicator from './components/TypingIndicator.vue';
 let drone = null
 
 function connectToScaledrone() {
-  drone = new window.Scaledrone('YOUR-CHANNEL-ID', {
+  drone = new window.Scaledrone('dpo6z5OXHf0Eombx', {
     data: me.value,
   });
   drone.on('open', error => {
@@ -49,27 +49,10 @@ onMounted(() => {
 });
 
 function randomName() {
-  const adjectives = [
-    'autumn', 'hidden', 'bitter', 'misty', 'silent', 'empty', 'dry', 'dark',
-    'summer', 'icy', 'delicate', 'quiet', 'white', 'cool', 'spring', 'winter',
-    'patient', 'twilight', 'dawn', 'crimson', 'wispy', 'weathered', 'blue',
-    'billowing', 'broken', 'cold', 'damp', 'falling', 'frosty', 'green', 'long',
-    'late', 'lingering', 'bold', 'little', 'morning', 'muddy', 'old', 'red',
-    'rough', 'still', 'small', 'sparkling', 'shy', 'wandering',
-    'withered', 'wild', 'black', 'young', 'holy', 'solitary', 'fragrant',
-    'aged', 'snowy', 'proud', 'floral', 'restless', 'divine', 'polished',
-    'ancient', 'purple', 'lively', 'nameless'
-  ];
+  const adjectives = [];
+  adjectives[0]=prompt("What is your name?","Insert here")
   const nouns = [
-    'waterfall', 'river', 'breeze', 'moon', 'rain', 'wind', 'sea', 'morning',
-    'snow', 'lake', 'sunset', 'pine', 'shadow', 'leaf', 'dawn', 'glitter',
-    'forest', 'hill', 'cloud', 'meadow', 'sun', 'glade', 'bird', 'brook',
-    'butterfly', 'bush', 'dew', 'dust', 'field', 'fire', 'flower', 'firefly',
-    'feather', 'grass', 'haze', 'mountain', 'night', 'pond', 'darkness',
-    'snowflake', 'silence', 'sound', 'sky', 'shape', 'surf', 'thunder',
-    'violet', 'water', 'wildflower', 'wave', 'water', 'resonance', 'sun',
-    'wood', 'dream', 'cherry', 'tree', 'fog', 'frost', 'voice', 'paper', 'frog',
-    'smoke', 'star'
+    ''
   ];
   const adjective = adjectives[Math.floor(Math.random() * adjectives.length)];
   const noun = nouns[Math.floor(Math.random() * nouns.length)];
@@ -77,7 +60,7 @@ function randomName() {
 }
 
 function randomColor() {
-  return '#' + Math.floor(Math.random() * 0xFFFFFF).toString(16);
+  return prompt("whats your color?", "lime")
 }
 
 const messages = ref([]);
